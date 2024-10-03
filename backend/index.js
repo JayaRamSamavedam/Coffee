60acbc7b8aa9705a32ab7edfde45d1f8cb203226
JayaRamSamavedam <98570026+JayaRamSamavedam@users.noreply.github.com>
2024-10-03 12:52:09 +0530

    latest

--- backend/index.js
+++ backend/index.js
@@ -47,7 +47,7 @@ app.use(limiter);
 
 // CORS configuration
 const corsOptions = {
-    origin:[process.env.CLIENT,'https://coffee-frontend-kappa.vercel.app/'], // specify your allowed origins
+    origin:['https://coffee-frontend-kappa.vercel.app'], // specify your allowed origins
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
 };
